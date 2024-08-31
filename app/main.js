eed9c488adaba09be00063d23780eb2bebeb35ec
joel <joel@burgerjoe.dev>
2024-08-31 17:33:14 +1200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,12 +1,20 @@
 const net = require('net');
 
+// refer to https://nodejs.org/docs/latest-v20.x/api/net.html
+
 const server = net.createServer((connection) => {
-  connection.addListener('data', (data) => {
-    console.log('Received ', data.toString());
-    connection.write('+PONG\r\n');
+  connection.on('data', (data) => {
+    connection.write('+PONG\r\n'); // Send a response back to the client
+  });
+  connection.on('error', (err) => {
+    console.log('Connection error', err);
   });
 });
 
-server.listen(6379, '127.0.0.1');
+server.on('error', (err) => {
+  console.log('Server error', err);
+});
 
-console.log('Listening on port 6379...');
+server.listen(6379, '127.0.0.1', () => {
+  console.log(`Listening on ${server.address().address}:${server.address().port}`);
+});
