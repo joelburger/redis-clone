ce3c85337d129e3f9f4482f9d6ac71c8a6770e39
joel <joel@burgerjoe.dev>
2024-08-31 15:54:00 +1200

    respond to ping

--- app/main.js
+++ app/main.js
@@ -1,7 +1,10 @@
-const net = require("net");
+const net = require('net');
 
 const server = net.createServer((connection) => {
   // Handle connection
+  connection.write('+PONG\r\n');
 });
 
-server.listen(6379, "127.0.0.1");
+server.listen(6379, '127.0.0.1');
+
+console.log('Listening on port 6379...');
