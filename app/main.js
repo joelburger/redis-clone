f0863aa90f094d7df29b2d060309d59b64739093
joel <joel@burgerjoe.dev>
2024-08-31 21:49:50 +1200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,6 +1,10 @@
 const net = require('net');
 
-// refer to https://nodejs.org/docs/latest-v20.x/api/net.html
+// References:
+// https://nodejs.org/docs/latest-v20.x/api/net.html
+// https://redis.io/docs/latest/develop/reference/protocol-spec/#bulk-strings
+
+const commands = { PING: 'ping', ECHO: 'echo', SET: 'set', GET: 'get' };
 
 function parseRespBulkString(data) {
   const dataString = Buffer.from(data).toString('UTF-8');
@@ -10,20 +14,59 @@ function parseRespBulkString(data) {
   });
 }
 
-function handleData(connection, data) {
+function validateArguments(commandName, arguments, expectedArgumentCount) {
+  if (expectedArgumentCount === 0) {
+    return !arguments;
+  }
+
+  if (arguments.length !== expectedArgumentCount) {
+    throw new Error(`Invalid number of arguments for ${commandName}`);
+  }
+}
+
+function writeString(connection, stringValue) {
+  connection.write(`+${stringValue}\r\n`);
+}
+
+function handleData(connection, data, map) {
   const stringValues = parseRespBulkString(data);
-  if (stringValues.length === 2 && stringValues[0].toLowerCase() === 'echo') {
-    connection.write(`+${stringValues[1]}\r\n`);
-  } else if (stringValues.length === 1 && stringValues[0].toLowerCase() === 'ping') {
-    connection.write('+PONG\r\n');
-  } else {
-    throw new Error('Invalid data');
+  const [command, ...arguments] = stringValues;
+
+  switch (command.toLowerCase()) {
+    case commands.PING:
+      validateArguments(commands.PING, arguments, 0);
+      writeString(connection, 'PONG');
+      break;
+    case commands.ECHO:
+      validateArguments(commands.ECHO, arguments, 1);
+      const [echoValue] = arguments;
+      writeString(connection, echoValue);
+      break;
+    case commands.SET:
+      validateArguments(commands.SET, arguments, 2);
+      const [key, value] = arguments;
+      map[key] = value;
+      writeString(connection, 'OK');
+      break;
+    case commands.GET:
+      validateArguments(commands.GET, arguments, 1);
+      const [searchKey] = arguments;
+      if (map[searchKey]) {
+        writeString(connection, map[searchKey]);
+      } else {
+        connection.write('$-1\\r\\n');
+      }
+      break;
+    default:
+      throw new Error(`Invalid command: ${command}`);
   }
 }
 
 const server = net.createServer((connection) => {
+  const map = new Map();
+
   connection.on('data', (data) => {
-    handleData(connection, data); // Send a response back to the client
+    handleData(connection, data, map); // Send a response back to the client
   });
   connection.on('error', (err) => console.log('Connection error', err));
 });
