4800720acedcc99364b58b4256093cb40b644a0f
joel <joel@burgerjoe.dev>
2024-08-31 19:29:28 +1200

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -2,18 +2,33 @@ const net = require('net');
 
 // refer to https://nodejs.org/docs/latest-v20.x/api/net.html
 
+function parseRespBulkString(data) {
+  const dataString = Buffer.from(data).toString('UTF-8');
+
+  return dataString.split('\r\n').filter((component) => {
+    return component && !component.startsWith('*') && !component.startsWith('$');
+  });
+}
+
+function handleData(connection, data) {
+  const stringValues = parseRespBulkString(data);
+  if (stringValues.length === 2 && stringValues[0].toLowerCase() === 'echo') {
+    connection.write(`+${stringValues[1]}\r\n`);
+  } else if (stringValues.length === 1 && stringValues[0].toLowerCase() === 'ping') {
+    connection.write('+PONG\r\n');
+  } else {
+    throw new Error('Invalid data');
+  }
+}
+
 const server = net.createServer((connection) => {
   connection.on('data', (data) => {
-    connection.write('+PONG\r\n'); // Send a response back to the client
+    handleData(connection, data); // Send a response back to the client
   });
-  connection.on('error', (err) => {
-    console.log('Connection error', err);
-  });
+  connection.on('error', (err) => console.log('Connection error', err));
 });
 
-server.on('error', (err) => {
-  console.log('Server error', err);
-});
+server.on('error', (err) => console.log('Server error', err));
 
 server.listen(6379, '127.0.0.1', () => {
   console.log(`Listening on ${server.address().address}:${server.address().port}`);
