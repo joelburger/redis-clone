b803aaad790ceea327800c6f5d780ba5a7628b8d
joel <joel@burgerjoe.dev>
2024-09-09 13:31:33 +1200

    codecrafters submit [skip ci]

--- app/commands/wait.js
+++ app/commands/wait.js
@@ -1,12 +1,13 @@
 const { commands } = require('../constants');
 const { validateArguments, isMaster } = require('../helpers/common');
 const { constructSimpleInteger } = require('../helpers/resp');
+const { REPLICAS } = require('../global');
 
 module.exports = {
   process(socket, args) {
     validateArguments(commands.WAIT, args, 2);
     if (isMaster()) {
-      socket.write(constructSimpleInteger(0));
+      socket.write(constructSimpleInteger(REPLICAS.length));
     }
   },
 };
