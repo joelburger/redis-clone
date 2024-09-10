75c1e9144d0ed2b6099d0267368453747c9dbc2b
joel <joel@burgerjoe.dev>
2024-09-10 21:31:07 +1200

    codecrafters submit [skip ci]

--- app/commands/wait.js
+++ app/commands/wait.js
@@ -1,15 +1,20 @@
 const { commands } = require('../constants');
-const { validateArguments, isMaster } = require('../helpers/common');
+const { validateArguments } = require('../helpers/common');
 const { constructSimpleInteger, constructArray } = require('../helpers/resp');
-const { REPLICAS, REPLICA_ACK, REPLICA_WAIT } = require('../global');
+const { REPLICAS, REPLICA_WAIT } = require('../global');
+
+let timeout;
+let requestedAck;
+let intervalId;
+let timeoutId;
 
 /**
  * Stops any active timers waiting for acknowledgments.
  * Clears both the interval and timeout if they are set.
  */
 function stopTimers() {
-  if (REPLICA_WAIT.intervalId) clearInterval(REPLICA_WAIT.intervalId);
-  if (REPLICA_WAIT.timeoutId) clearTimeout(REPLICA_WAIT.timeoutId);
+  if (intervalId) clearInterval(intervalId);
+  if (timeoutId) clearTimeout(timeoutId);
 }
 
 /**
@@ -19,10 +24,10 @@ function stopTimers() {
  * @param {object} socket - The client socket to respond to.
  */
 function checkAckAndRespond(socket) {
-  REPLICA_WAIT.intervalId = setInterval(() => {
-    if (REPLICA_WAIT.ack >= REPLICA_WAIT.requestedAck) {
+  intervalId = setInterval(() => {
+    if (REPLICA_WAIT.ack >= requestedAck) {
       stopTimers();
-      console.log(`Requested ack has been reached. Request was ${REPLICA_WAIT.requestedAck}`);
+      console.log(`Requested ack has been reached. Request was ${requestedAck}`);
       socket.write(constructSimpleInteger(REPLICA_WAIT.ack));
     }
   }, 1);
@@ -37,31 +42,32 @@ function checkAckAndRespond(socket) {
  * @param {object} socket - The client socket to respond to.
  */
 function setReplicaWaitTimeout(socket) {
-  console.log(`Setting replica wait timeout to ${REPLICA_WAIT.timeout}`);
-  REPLICA_WAIT.timeoutId = setTimeout(() => {
-    console.log(`Timeout reached. Returning ack value of ${REPLICA_WAIT.ack || REPLICAS.length}`);
+  console.log(`Setting replica wait timeout to ${timeout}`);
+  timeoutId = setTimeout(() => {
     stopTimers();
-    socket.write(constructSimpleInteger(REPLICA_WAIT.ack || REPLICAS.length));
-  }, REPLICA_WAIT.timeout);
+    const result = REPLICA_WAIT.ack || REPLICAS.length;
+    console.log(`Timeout reached. Returning ack value of ${result}`);
+    socket.write(constructSimpleInteger(result));
+  }, timeout);
 }
 
 /**
  * Sends an acknowledgment request to all replica sockets.
  */
-function sendAckToAllReplicas() {
+function sendAckRequestToAllReplicas() {
+  // Reset the acknowledgment count to zero before sending acknowledgment requests.
+  REPLICA_WAIT.ack = 0;
+
   REPLICAS.forEach((replicaSocket) => replicaSocket.write(constructArray(['REPLCONF', 'GETACK', '*'])));
 }
 
 module.exports = {
   process(socket, args) {
     validateArguments(commands.WAIT, args, 2);
-    REPLICA_WAIT.requestedAck = parseInt(args[0], 10);
-    REPLICA_WAIT.timeout = parseInt(args[1], 10);
+    requestedAck = parseInt(args[0], 10);
+    timeout = parseInt(args[1], 10);
 
-    // Reset ack counter
-    REPLICA_WAIT.ack = 0;
-
-    sendAckToAllReplicas();
+    sendAckRequestToAllReplicas();
 
     //The WAIT command should return when either
     // (a) the specified number of replicas have acknowledged the command,
