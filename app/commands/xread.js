d2ceae9b0867d6877ba4f36be542def522460eba
joel <joel@burgerjoe.dev>
2024-09-15 10:27:56 +1200

    codecrafters submit [skip ci]

--- app/commands/xread.js
+++ app/commands/xread.js
@@ -29,38 +29,67 @@ function fetchStream(streamKey) {
   return STORAGE.has(streamKey) ? STORAGE.get(streamKey) : { value: new Set(), type: 'stream' };
 }
 
+/**
+ * Sends data to the socket based on the provided queries.
+ * For each query, it fetches the stream associated with the stream key,
+ * filters the entries based on the stream ID, and constructs the response.
+ * If there is data to send, it writes the constructed array to the socket.
+ *
+ * @param {Object} socket - The socket to write data to.
+ * @param {Array} queries - The queries containing stream keys and IDs.
+ * @returns {boolean} - Returns true if data was sent, otherwise false.
+ */
 function sendData(socket, queries) {
   const result = queries.map((query) => {
     const stream = fetchStream(query.streamKey);
     const subResult = Array.from(stream.value)
       .filter((entry) => compareStreamId(query.streamId, entry.streamId) > 0)
       .map((entry) => {
+        // Ensures that the `streamId` in the query reflects the most recent entry
+        // processed, which is important for subsequent filtering and processing
+        // of stream entries.
         query.streamId = entry.streamId;
+
         return constructArray([constructString(entry.streamId), constructBulkStringArray(entry.data)]);
       });
     return constructArray([constructString(query.streamKey), constructArray(subResult)]);
   });
 
-  socket.write(constructArray(result));
+  if (result.length) {
+    socket.write(constructArray(result));
+    return true;
+  }
+
+  return false;
+}
+
+function continueToWait(blockTimeout, startTime) {
+  return blockTimeout === 0 || startTime + blockTimeout > Date.now();
 }
 
 module.exports = {
   async process(socket, args) {
     validateArguments(commands.XREAD, args, 3);
 
-    const blockTimeout = args[0].toLowerCase() === 'block' ? Number(args[1]) : undefined;
-    const keysAndIds = args.slice(blockTimeout ? 3 : 1);
+    const blockEnabled = args[0].toLowerCase() === 'block';
+    const blockTimeout = blockEnabled ? Number(args[1]) : undefined;
+    const keysAndIds = args.slice(blockEnabled ? 3 : 1);
     const queries = parseQuery(keysAndIds);
 
-    if (blockTimeout) {
-      // Continuously check for new data until the block timeout is reached
+    if (blockEnabled) {
+      // Continuously check for new data until the block timeout is reached,
+      // or stream data has been sent.
       const startTime = Date.now();
-      while (startTime + blockTimeout > Date.now()) {
+      while (continueToWait(blockTimeout, startTime)) {
         // Reduce polling frequency by pausing execution for 2.5 seconds
         await sleep(2500);
-        sendData(socket, queries);
+
+        // If the block timeout is 0 (waiting indefinitely), exit the function once data
+        // has been successfully sent.
+        if (sendData(socket, queries) && blockTimeout === 0) return;
       }
-      // Write a NIL value to the socket if the block timeout is reached
+      // Write a NIL value to the socket if the block timeout is reached and
+      // no stream data has been sent to the client.
       socket.write(NIL_VALUE);
     } else {
       sendData(socket, queries);
